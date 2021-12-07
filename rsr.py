a65bf9893864ce5fbfdf3adb5e985e56b932b897
usiqwerty <benlogin1@protonmail.com>
2021-12-07 15:49:18 +0500

    Minor optimizations Renamed: walkthrough() -> olymps() Minor comments

--- rsr.py
+++ rsr.py
@@ -6,38 +6,41 @@ from bs4 import BeautifulSoup
 
 
 
-def walkthrough(upd, subj, lvl):
-	output=list()
+def olymps(upd, subj, lvl):
+	output=[]
 	lastprintname=""
 	if upd:
 		print ("Fetching data...")
 		html=requests.get("https://rsr-olymp.ru").text
 		tables.html2csv('rsr-olymp.csv', html)
-	with open('rsr-olymp.csv', newline='') as csvfile:
+	with open('rsr-olymp.csv', newline='', encoding='utf8') as csvfile:
 		rd=csv.reader(csvfile, delimiter=',', quotechar='"')
 		for row in rd:
-			id=row[0]
+			#id=row[0]
 			name=row[1]
-			theme=row[2]
+			#theme=row[2]
 			subject=row[3]
 			level=row[4]
+
 			if name!= "":
+				#if this is next olympiad
 				lastname=name
 			else:
 				name=lastname
 			if ( level==str(lvl) or lvl==0 )and subject.find(subj)>=0:
+				#if this is the one we need
+
 				if lastprintname!=name:
+					#and it wasn't printed before
 					output.append(name)
-					#print (name)
 					lastprintname=name
 	return output
-#def get(lvl=0,subj='',upd=False):
 
 if __name__=="__main__":
 	args=sys.argv
-	level=0 #default
+	level=0
 	subject=''
-	result=list()
+	result=[]
 
 	upd=False
 	for i in args:
@@ -47,10 +50,12 @@ if __name__=="__main__":
 			level=2
 		elif i=="III":
 			level=3
-		elif i!="--update" and i!='' and i!= sys.argv[0]:
-			subject=i
 		elif i=="--update":
 			upd=True
-	for i in walkthrough(upd, subject, level):
+		elif i!= sys.argv[0]:
+			subject=i
+
+	#standalone wrapper
+	for i in olymps(upd, subject, level):
 		print(i)
 
