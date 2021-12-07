7067c9807128fae459fc7cff1674065ba46304ad
usiqwerty <benlogin1@protonmail.com>
2021-12-07 16:40:30 +0500

    Olymps are only processed if user said yes Before 'for i in olymps' and olymps list definition was outside 'if' block

--- vuz.py
+++ vuz.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
-import egevuz,rsr,sgo
+import egevuz,rsr,grades
+
+marks=['']*11
 
 print("vuz abobus by usiqwerty/2021")
+
+
+
 subjs=['математика','русский язык','физика','общест','история','биология', 'информа', 'химия', 'литература', 'география', 'иностран']
+
 for i in range(len(subjs)):
 	print(i+1, subjs[i])
-marks=['']*11
 choice=input("Выберите предметы через пробел: ")
 ol=input("Олимпиады надо? y/n: ")
-#d={'mat':'','rus':'','fiz':'','obshe':'','ist':'','biol':'','inform':'','him':'','liter':'','georg':'','inyaz':''}
-#print(d)
 
 for i in choice.split(" "):
-	print(int(i)-1, subjs[int(i)-1])
-	school=float(sgo.get_average(subjs[int(i)-1]))
+	school=float(grades.get_average(subjs[int(i)-1]))
 	ege=(school-2)*33.3
 	marks[int(i)-1]=round(ege)
 print("\nВаши баллы на ЕГЭ предположительно будут такими: ", list(filter(lambda x: x!='', marks)))
 print("На основе этих баллов, вы можете попасть в один из этих вузов:\n")
-egevuz.vuzopedia(marks, 83)
 
-olymps=set()
-if ol=="y":
-	oly=True
+
+egevuz.vuzopedia(marks, 83)
+if ol in ["y", "д"] :
+	#we are using set instead of list
+	#to prevent duplicates
+	olymps=set()
 	print("\nА так же олимпиады:")
 	for i in choice.split(" "):
-		school=float(sgo.get_average(subjs[int(i)-1]))
+		school=float(grades.get_average(subjs[int(i)-1]))
 		ege=(school-2)*33.3
 		if ege>=75:
-			for i in rsr.walkthrough(upd=False, subj=subjs[int(i)-1], lvl=1):
+			for i in rsr.olymps(upd=False, subj=subjs[int(i)-1], lvl=1):
 				olymps.add(i)
-for i in olymps:
-	print(i)
+	for i in olymps:
+		print(i)
 print("Удачи ;)")
