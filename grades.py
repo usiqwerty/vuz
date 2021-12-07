013b9308eba729d2e4dc16c87e996b94d3f30df0
usiqwerty <benlogin1@protonmail.com>
2021-12-07 16:02:54 +0500

    Renamed sgo.py -> grades.py Commented

---
+++ grades.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys, tables, csv
+
+def get_average(subject):
+	with open('grades.csv', newline='', encoding='utf8') as csvfile:
+		rd=csv.reader(csvfile, delimiter=',', quotechar='"')
+		for row in rd: #iterate until subject was found, else return 0
+			last=len(row)-1
+			avg=row[last]
+			if row[0].lower().find(subject)>=0:
+				return avg.replace(',', '.')
+				#we need to decimal delimeter to point
+				#to make python able to convert string into float
+	return 0
+
+if __name__=="__main__":
+	for i in range(len(sys.argv)):
+		if sys.argv[i]=="--update":
+			print("Running database update...")
+			f=open('sgo.xls')
+			html=f.read()
+			f.close()
+			tables.html2csv('grades.xls', html)
