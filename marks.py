db4a9fb17d3835f1e525cf574f70e3fbf5e0f357
usiqwerty <benlogin1@protonmail.com>
2021-12-24 21:32:43 +0500

    Fully rewritten

--- marks.py
+++ marks.py
@@ -1,89 +1,91 @@
 #!/usr/bin/env python3
 marks=list()
-cc=0
-ss=0
 
-print ("Marks perspective calculator by usiqwerty")
-while True:
-    i=int(input())
-    marks.append(i)
-    if i !=9:
-        cc+=1
-        ss+=i
-    else:
+def test(sum, count):
+	result=[]
 
-        break
-print ("Starting...")
-was=float(ss)/float(cc)
-print ("Current average:", was, " with ", cc, " marks")
-def test(c, s):
-    test5ing=0
-    test45ing=0
-    test455ing=0
-    dc=c
-    ds=s
-    while test5ing < 4.5:
-        s+=5
-        c+=1
-        test5ing=float(s)/float(c)
-    print ("fives: ",c-dc, " times more")
-    c=dc
-    s=ds
-    for i in range(10):
-        s+=9
-        c+=2
-        test45ing=float(s)/float(c)
-        if test45ing>=4.5:
-            print ("four-fives: ", c-dc, " times more")
-    if test45ing<4.5:
-        print ("four-five combination is not possible")
-    c=dc
-    s=ds
-    while test455ing < 4.5:
-        s+=14
-        c+=3
-        test455ing=float(s)/float(c)
-    print ("four-five-fives: ", c-dc, " times more")
+	test=0
 
-if was<4.5:
-    test(cc, ss)
-else:
-    print("Already five")
-print("==== Emergency calculations ====")
-test(cc+1, ss+2)
-print ("==== Degrade calculation ====")
-test4ing=float(ss)/float(cc)
-s=ss
-c=cc
-while test4ing>=4.5:
-    s+=4
-    c+=1
-    test4ing=float(s)/float(c)
-print ("fours: ", c-cc, " times more")
-s=ss
-c=cc
-test3ing=float(ss)/float(cc)
-while test3ing>=4.5:
-    s+=3
-    c+=1
-    test3ing=float(s)/float(c)
-print ("threes: ", c-cc, " times more")
-s=ss
-c=cc
-test2ing=float(ss)/float(cc)
-while test2ing>=4.5:
-    s+=2
-    c+=1
-    test2ing=float(s)/float(c)
-print ("twos: ", c-cc, " times more")
-s=ss
-c=cc
-for i in range(10):
-        s+=9
-        c+=2
-        test45ing=float(s)/float(c)
-        if test45ing>=4.5:
-            print ("four-fives: ", c-cc, " times more")
-            break
-if test45ing<4.5:
-    print ("four-five combination is not possible")
+	s=sum
+	c=count
+	adv=0
+
+	if s/c>=4.5:
+		result=[-1]
+		return result
+	while test<4.5:
+		s+=5
+		adv+=1
+		test=s/(c+adv)
+		if adv>7:
+			adv=-1
+			break
+	result.append(adv)
+
+	s=sum
+	c=count
+	adv=0
+	while test<4.5:
+		s+=4+5
+		adv+=2
+		test=s/(c+adv)
+		if adv>14:
+			adv=-1
+			break
+	result.append(adv)
+
+	s=sum
+	c=count
+	adv=0
+	while test<4.5:
+		s+=4+5+5
+		adv+=3
+		test=s/(c+adv)
+		if adv>21:
+			adv=-1
+			break
+	result.append(adv)
+	return result
+def deg_test(sum, count):
+	result=[]
+	test=0
+	for i in [2, 3, 4]:
+		s=sum
+		c=count
+		test=s/c
+		adv=0
+		while test>=4.5:
+			s+=i
+			adv+=1
+			test=s/(c+adv)
+		result.append(adv)
+	return result
+def calculate(marks):
+	count_init=0
+	sum_init=0
+	for j in marks:
+		sum_init+=j
+		count_init+=1
+	avg=sum_init/count_init
+	rise=test(sum_init, count_init)
+	degrade=deg_test(sum_init, count_init)
+	return (avg, rise, degrade)
+
+if __name__=="__main__":
+	print ("Marks perspective calculator by usiqwerty")
+	i=input()
+	count_init=0
+	sum_init=0
+	for j in i.split(" "):
+		sum_init+=int(j)
+		count_init+=1
+	rise=test(sum_init, count_init)
+	print ( "Current average: {} with {} marks".format(sum_init/count_init, count_init) )
+	print("===")
+	if rise[0]==-1:
+		print("Already five")
+	else:
+		print("Fives: {}\nFour-fives: {}\nFour-five-fives: {}".format(*rise))
+	print("===")
+	degrade=deg_test(sum_init, count_init)
+	print("Twos: {}\nThrees: {}\nFours: {}".format(*degrade))
