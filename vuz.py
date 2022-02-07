49ffaa213e58ad53277ee4693a5e34aa417dd731
usiqwerty <benlogin1@protonmail.com>
2022-02-07 18:31:41 +0500

    cli options avaible again

--- vuz.py
+++ vuz.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import sys
 import grades, egevuz, rsr
 subjs=['Математика','Русский язык','Физика','Обществознание','История','Биология', 'Информатика', 'Химия', 'Литература', 'География', 'Иностранный язык']
 cities={
@@ -13,34 +14,33 @@ cities={
 	20:'Махачкала'
 }
 
-oly=False
-advanced_olymps=False
-city=0
-
-#def main ():
-
-
 if __name__=="__main__":
+	city=0
+	for i in sys.argv:
+		try:
+			city=int(i)
+		except:
+			if i=="--update":
+				rsr.update()
+			continue
 	print ("vuz rewritten v2.0")
-	#print("testing 2.0")
 
 #subjects
 	for i in range(len(subjs)):
 		print (str(i+1), "\t"+subjs[i])
 	subjs_input=map( lambda x: int(x)-1 , input("Предметы через пробел: ").split(' ') )
 	subs=list(subjs_input)
-	print (subs)
-	#subs=[0,1,6]
 ###
 
 #cities
-	for i in cities:
-		print(i,"\t"+ cities[i])
-
-	city=int(input("Код города: "))
-	print(cities[city])
-	olmps = True if input("Олимпиады y/n? ")=='y' else False
+	if city==0:
+		for i in cities:
+			print(i,"\t"+ cities[i])
+		city=int(input("Код города: "))
+		print(cities[city])
 ###
+	olmps = True if input("Олимпиады y/n? ")=='y' else False
+
 
 #EGE score
 	grades=grades.get_grades(list(map(lambda x: subjs[x],subs)))
@@ -51,7 +51,6 @@ if __name__=="__main__":
 		except:
 			continue
 		grades[i]=round((curr-2)*33.3)
-	print(grades)
 ###
 
 #vuzopedia
@@ -61,7 +60,6 @@ if __name__=="__main__":
 		if subjs[i] in grades:
 			score=str(grades[subjs[i]])
 		scores.append(score)
-	print(scores)
 	url="https://vuzopedia.ru/vuzfilter?vuz=&mat={}&rus={}&fiz={}&obshe={}&ist={}&biol={}&inform={}&him={}&liter={}&georg={}&inyaz={}&city[]={}".format(*scores, city)
 	vuzes=egevuz.vuzopedia(url)
 	for i in vuzes:
@@ -72,7 +70,6 @@ if __name__=="__main__":
 	if olmps:
 		olymps=set()
 		for sub in filter(lambda x: int(scores[x])>=75, subs):
-			print(subjs[sub])
 			for i in rsr.olymps(subjs[sub].lower(), 1):
 				olymps.add(i)
 		for i in olymps:
