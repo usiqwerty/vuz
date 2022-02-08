4df506923112310ad8fbe0fdb88069cb8d3c20a8
usiqwerty <benlogin1@protonmail.com>
2022-02-08 14:31:43 +0500

    Bugfix: if olympiads were not reqested, printer.py tried to iterate empty list.

--- printer.py
+++ printer.py
@@ -8,21 +8,19 @@ def printout(vuzes, olymps, marks, version):
 	line="---------"+str(marks)+"----------------------------------"
 	file +=line+"\\par\n"
 	for i in vuzes:
-		#i=str(bytes(i,'utf-16'))[1:].replace('x','u')
-		#i = str(bytes(i,'utf-8'), 'cp1251')
 		i=str(i.encode('rtfunicode'))[1:].replace('\\\\','\\')
 		file +="{\\ql "+ i +" \\q0}\\par\n"
 
 	file +="-"*len(line) +"\par\n"
 
-	for i in olymps:
-		olymp=i.split("http")[0]
-		link="http" + i.split("http")[1]
-		#olymp=str(bytes(olymp,'utf-16'))[1:].replace('x','u')
-		#olymp=str(bytes(olymp, 'utf-8'), 'cp1251')
-		olymp=str(olymp.encode('rtfunicode'))[1:].replace('\\\\','\\')
-		file +="{\\ql "+ olymp +" \\q0}\\par\n"
-		file +="{\\qr "+ link +" \\q0}\\par\n"
+	if len(olymps)>1:
+		for i in olymps:
+			olymp=i.split("http")[0]
+			link="http" + i.split("http")[1]
+			olymp=str(olymp.encode('rtfunicode'))[1:].replace('\\\\','\\')
+			file +="{\\ql "+ olymp +" \\q0}\\par\n"
+			file +="{\\qr "+ link +" \\q0}\\par\n"
+
 	file+="\n}}"
 	file=file.replace("'", "")
 
