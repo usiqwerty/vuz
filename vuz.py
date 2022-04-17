eba1041ec15148d9ba45caaf19560e7a103842fe
usiqwerty <benlogin1@protonmail.com>
2022-04-17 19:52:47 +0500

    GPLv3 License

--- vuz.py
+++ vuz.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+vuz - command line tool to search univercities
+Copyright (C) 2021-2022  usiqwerty
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import sys
 import grades, egevuz, rsr, printer
 version="volk ubil zaitsa v3.1"
@@ -54,6 +72,12 @@ themes={
 37: "Философия и религия",
 38: "Нанотехнологии и наноматериалы"}
 if __name__=="__main__":
+	print("Copyright (C) 2021-2022 usiqwerty")
+	print("This program comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file")
+	print("This is free software, and you are welcome to redistribute it")
+	print("under certain conditions; see LICENSE for details.")
+
+
 	city=0
 	printout=False
 	deep=False
