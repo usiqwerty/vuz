24249736fdf914eb4cf1400d8d69bb15a1497050
usiqwerty <benlogin1@protonmail.com>
2022-06-17 19:16:09 +0500

    vuzopeida() will iterate until reaches end. Total numbers of page isn't known Minor optimisations

--- egevuz.py
+++ egevuz.py
@@ -1,21 +1,4 @@
 #!/usr/bin/env python3
-"""
-vuz - command line tool to search univercities
-Copyright (C) 2021-2022  usiqwerty
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
 
 #TODO:make dup-check
 import requests, re
@@ -24,15 +7,16 @@ from bs4 import BeautifulSoup
 p=['█', '▓', '▒','░']
 
 def deepscan(id, city):
-	dict_vuzes=dict()
+	#dict_vuzes=dict() #anti-dup
 	result_vuzes=list()
-	print(city)
+
 	try:
 		if city=='':
 			url=f"https://vuzopedia.ru/program/bakispec/{id}/varianty"
 		else:
+			#print(f"City: {city}")
 			url=f"https://vuzopedia.ru/region/city/{city}/program/bakispec/{id}/varianty"
-		print(f"{city} Deepscanning", id)
+		print(f"[{city}] Deepscanning", id)
 		html=requests.get(url).text
 		soup=BeautifulSoup(html, 'html.parser')
 		listed_vuzes=soup.find_all("a")
@@ -41,67 +25,84 @@ def deepscan(id, city):
 				title=i.find("div", class_="itemVuzTitle")
 				if title:
 					title=title.text.strip()
-				else:
+				else: #sus, maybe delete
 					continue
-				if title not in dict_vuzes:
-					dict_vuzes[title]=re.findall(r"[0-9]+",i.get("href"))[0]
-				result_vuzes.append(dict_vuzes[title])
+				#if title not in dict_vuzes:
+				#dict_vuzes[title]=re.findall(r"[0-9]+",i.get("href"))[0]
+
+				result_vuzes.append(re.findall(r"[0-9]+",i.get("href"))[0]) #no anti-dup
 
 	except requests.exceptions.ConnectionError:
-		result_vuzes.append(-1)
+		#result_vuzes.append(-1)
+		return #None
 
 	return(result_vuzes)
 
-def vuzopedia(scores, city, theme, deep, hothead):
-	page=1
-	total=1
-	current=0
+def vuzopedia(scores, city, theme, deep): #, hothead
+	page=1 #for multi-page url
+	#total=1	#maximum number of pages, it will be set later
+	#current=0
+
 	result_vuzes=list()
-	vuzes=list()
-	if hothead:
+	#vuzes=list() kuk?
+	if city=='':
 		print("HOTHEAD")
 		citystr=''
 	else:
 		citystr=f"&city[]={city}"
 
-	while current<total:
-		msg=f'Processing: page {page} of {total} \t'
-		print(msg+p[3]*round(80), end='\r')
-		print(msg+p[0]*round(80*page/total), end='\r')
+	while True:
+		#msg=
+		#print(msg+p[3]*round(80), end='\r')
+		#print(msg+p[0]*round(80*page/total), end='\r') #super mega cool progressbar
+		print(f'Processing: page {page}')
 
 		url="https://vuzopedia.ru/vuzfilter/prog?vuz=&obshezh=&voenkaf=&budzh=&gosu=&theme={}&och=&zaoch=&ochzaoch=&distans=&vstupisp=&idcmb=&page={}&mat={}&rus={}&fiz={}&obshe={}&ist={}&biol={}&inform={}&him={}&liter={}&georg={}&inyaz={}".format(theme, page, *scores)
 		url+=citystr
+		#print (url)
 		try:
 			html=requests.get(url).text
 			soup=BeautifulSoup(html, 'html.parser')
-			vuz_list=soup.find_all("a", class_="spectittle")
+			links_list=soup.find_all("a", class_="spectittle") #wait here's typo in classname, why does it work?!
 
-			for i in vuz_list:
+			for i in links_list:
 				id=re.findall( r"[0-9]+", i.get("href"))[0]
-				ans= [ str(id), i.text.strip() ]
+				ans= [ str(id), i.text.strip() ] #specid and spectitle
 				if deep:
-					scanned = deepscan(id, city if not hothead else '')
+					scanned = deepscan(id, city) # if not hothead else ''
 					if not scanned:
 						continue
-					ans += scanned
-					#ans.append ( str(scanned) )
-				result_vuzes.append(ans)
+					ans += scanned #merge arrays
+				result_vuzes.append(ans) #save to result
 
-			if total==1:	#if we'll find out that it is really one, then
-					#this loop won't run again,
-					#because 'current' and 'total' would be equal
-				pagination=soup.find("ul", class_="pagination")
-				if pagination:
-					pages=pagination.find_all("li")
-					total=len(pages)
+			#if total==1:	#if we'll find out that it is really one, then
+					#we'd break out of the loop
+			pagination=soup.find("ul", class_="pagination")
+			if pagination:
+				pages=pagination.find_all("li")
+				if len(pages) <=3:
+					break #last page
+			else:
+				print("No page links found, parsing stopped")
+				break
+				#if total==1:
+				#	break
+
 
 		except requests.exceptions.ConnectionError:
-			result_vuzes.append(-1)
+			####TODO: show results even on disconnect
+			####result_vuzes.insert(0, -1)
+			####break
+
+			#just error, if something was found before loosing connection
+			#we'd loose it
+			return [-1] #return, since it's no need to iterate any further
+
+		#current+=1
 		page+=1
-		current+=1
 		if page%10==0:
 			sleep(5)
 		else:
 			sleep(1)
-	print("")
+	#print("")
 	return result_vuzes
--- vuz.py
+++ vuz.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python3
-"""
-vuz - command line tool to search univercities
-Copyright (C) 2021-2022  usiqwerty
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
+#TODO:printout seems to be very strange and obsolete, maybe i should be deleted
 import sys, os, csv
 import grades, egevuz, rsr, printer, wordgroup
 version="volk ubil zaitsa v4.0dev"
@@ -33,163 +16,175 @@ cities={
 	20:'Махачкала'
 }
 themes={
-1: "Управление и менеджмент",
-2: "Экономика и финансы",
-3: "Математика, информационные науки и технологии",
-4: "Образование и педагогика",
-5: "Искусство и культура",
-6: "Машиностроение, автоматизация и робототехника",
-7: "Транспорт: наземный, воздушный, водный",
-8: "Строительство, архитектура и недвижимость",
-9: "Юриспруденция",
-10: "Химико-биологические науки и технологии",
-11: "Науки о земле, геология и геодезия",
-12: "Энергетика и электротехника",
-13: "Филология и лингвистика",
-14: "Экология, природообустройство и безопасность",
-15: "СМИ, журналистика, реклама и PR",
-16: "Психология",
-17: "Электроника, связь и радиотехника",
-18: "Сельское, лесное и рыбное хозяйство",
-19: "Сервис, туризм и гостиничное дело",
-20: "Дизайн",
-21: "Физико-технические науки и технологии",
-22: "Технологии легкой и пищевой промышленности",
-23: "Технологические машины, оборудование и спецтранспорт",
-24: "Медицина и здравоохранение",
-25: "Качество и управление в технических системах",
-26: "Логистика",
-27: "Политика и международные отношения",
-28: "Торговля и товароведение",
-29: "Управление персоналом",
-30: "История, археология и документоведение",
-31: "Социология и социальная работа",
-32: "Физическая культура, спорт и фитнес",
-33: "Технологии материалов и металлургия",
-34: "Маркетинг",
-35: "Приборостроение, оптика и биотехника",
-36: "Оружие и системы вооружения",
-37: "Философия и религия",
-38: "Нанотехнологии и наноматериалы"}
+	1: "Управление и менеджмент",
+	2: "Экономика и финансы",
+	3: "Математика, информационные науки и технологии",
+	4: "Образование и педагогика",
+	5: "Искусство и культура",
+	6: "Машиностроение, автоматизация и робототехника",
+	7: "Транспорт: наземный, воздушный, водный",
+	8: "Строительство, архитектура и недвижимость",
+	9: "Юриспруденция",
+	10: "Химико-биологические науки и технологии",
+	11: "Науки о земле, геология и геодезия",
+	12: "Энергетика и электротехника",
+	13: "Филология и лингвистика",
+	14: "Экология, природообустройство и безопасность",
+	15: "СМИ, журналистика, реклама и PR",
+	16: "Психология",
+	17: "Электроника, связь и радиотехника",
+	18: "Сельское, лесное и рыбное хозяйство",
+	19: "Сервис, туризм и гостиничное дело",
+	20: "Дизайн",
+	21: "Физико-технические науки и технологии",
+	22: "Технологии легкой и пищевой промышленности",
+	23: "Технологические машины, оборудование и спецтранспорт",
+	24: "Медицина и здравоохранение",
+	25: "Качество и управление в технических системах",
+	26: "Логистика",
+	27: "Политика и международные отношения",
+	28: "Торговля и товароведение",
+	29: "Управление персоналом",
+	30: "История, археология и документоведение",
+	31: "Социология и социальная работа",
+	32: "Физическая культура, спорт и фитнес",
+	33: "Технологии материалов и металлургия",
+	34: "Маркетинг",
+	35: "Приборостроение, оптика и биотехника",
+	36: "Оружие и системы вооружения",
+	37: "Философия и религия",
+	38: "Нанотехнологии и наноматериалы"
+}
 if __name__=="__main__":
-	print("Copyright (C) 2021-2022 usiqwerty")
-	print("This program comes with ABSOLUTELY NO WARRANTY; for details see LICENSE file")
-	print("This is free software, and you are welcome to redistribute it")
-	print("under certain conditions; see LICENSE for details.")
-
 
 	city=0
 	printout=False
 	deep=False
 	hothead=False
 	verbose_vuzes=False
-	for i in sys.argv:
+	force=False
+	for arg in sys.argv:
 		try:
-			city=int(i)
+			city=int(arg)
 		except:
-			if i=="--update":
+			if arg=="--update":
 				rsr.update()
-			elif i=="--print":
+			elif arg=="--print":
 				printout=True
-			elif i=="--deep":
+			elif arg=="--deep":
 				deep=True
-			elif i=="--verbose":
+			elif arg=="--verbose":
 				verbose_vuzes=True
-			elif i=="--hothead":
+			elif arg=="--hothead":
 				hothead=True
-			elif i=="--help":
+			elif arg=="--force":
+				print("--force option is not implemented")
+				force=True
+			elif arg=="--help":
 				print()
-				print(f"{sys.argv[0]} [city_id] [--update] [--print] [--deep] [--verbose]")
+				print(f"{sys.argv[0]} [city_id] [--update] [--print] [--deep] [--verbose] [--force]")
 				quit()
 			continue
 	print ("vuz", version)
 
-#subjects
+	#iterate through subjects and print them
 	for i in range(len(subjs)):
-		print (str(i+1), "\t"+subjs[i])
-	subjs_input=map( lambda x: int(x)-1 , input("Предметы через пробел: ").split(' ') )
-	subs=list(subjs_input)
-	#subs=[0,1,6]
-###
+		print (str(i+1), "\t"+subjs[i]) #i-th subject
+	subjs_input=map( lambda x: int(x)-1 , input("Предметы через пробел: ").split(' ') ) #get int's from string and make index from number
+	subs=list(subjs_input) #make list from map object
+	###
 
-#cities
-	if city==0:
+	#if city's not specified in cmdline
+	if city==0 and not hothead:
 		for i in cities:
 			print(i,"\t"+ cities[i])
 		city=int(input("Код города: "))
-		print(cities[city])
-	#city=83
-###
+		print(cities[city]) #choice
+	elif hothead:
+		city=''
+	###
 	olmps = True if input("Олимпиады y/n? ")=='y' else False
-	#olmps=True
 
 
-#EGE score
-	grades=grades.get_grades(list(map(lambda x: subjs[x],subs)))
+
+	#generate EGE score
+	subjects=list(map(lambda x: subjs[x],subs)) #get list of subjects by specified indexes
+	grades=grades.get_grades(subjects) #get grades by suject names
 	for i in grades:
-		try:
+		try:	#translate decimal point and convert to float
 			curr=float(grades[i].replace(',','.'))
-		except:
+			grades[i]=round((curr-2)*33.3)
+		except: #cell could be empty
 			continue
-		grades[i]=round((curr-2)*33.3)
-###
-#themes
+	###
+
+	#themes
 	for i in themes:
 		print(i,"\t"+ themes[i])
 	theme=int(input("Тематика: "))
 	print(themes[theme])
-###
-	if printout and not deep and not verbose_vuzes:
+	###
+	if printout and not deep and not verbose_vuzes: #deep and verbose modes are restricted to be printed
 		verbose_vuzes = True if input("Прописать названия вузов в дополнение к их номерам y/n? ")=="y" else False
-#vuzopedia
+
+	#fetch vuzes
 	scores=[]
-	for i in range(len(subjs)):
+	#for i in range(len(subjs)):
+	for subj in subjs:
 		score=''
-		if subjs[i] in grades:
-			score=str(grades[subjs[i]])
+		#if subjs[i] in grades:
+		if subj in grades:
+			#score=str(grades[subjs[i]])
+			score=str(grades[subj])
 		scores.append(score)
 	#scores=['80', '80', '', '', '', '', '80', '', '', '', '']
-	#print(scores)
-	vuzes=egevuz.vuzopedia(scores, city, theme, deep, hothead)
-	string_vuzes = list(map(' '.join, vuzes))
+	results = egevuz.vuzopedia(scores, city, theme, deep) #, hothead
+	string_results = list(map(' '.join, results)) #make string from each item of 'results' and put into list
+	if results[0]==-1:
+		print("Нет интернета")
+	'''replaced with wordgroup
 	for i in vuzes:
 		if i==-1:
 			print("Нет интернета")
 			quit()
-		#print(i) wordgroup will do it
-###
+		print(i)
+	'''
+	###
 	print("https://vuzopedia.ru/program/bakispec/<id>")
-#olympiads
+	#olympiads
 	if olmps:
 		olymps=set()
-		for sub in filter(lambda x: int(scores[x])>=75, subs):
-			for i in rsr.olymps(subjs[sub].lower(), 1):
+		for subj in filter(lambda x: int(scores[x])>=75, subs): #get only subjects with enough EGE score
+			for i in rsr.olymps(subjs[subj].lower(), 1): #and get olympiads for these subjects
 				olymps.add(i)
 		for i in olymps:
 			print(i)
 	else:
 		olymps=['Поиск олимпиад не производился']
+	###
 	if printout:
 		print("Generating printout...")
-		if hothead:
-			city=''
-		else:
+		#if hothead: it was done before
+		#	city=''
+		if not hothead:
 			city=cities[city]
-		printer.printout(string_vuzes, olymps, list(filter(lambda x: x!='', scores)), version, egevuz.dict_vuzes, verbose_vuzes, city)
-###
+		printer.printout(string_results, olymps, list(filter(lambda x: x!='', scores)), version, egevuz.dict_vuzes, verbose_vuzes, city)
+
+	#save results
 	vuzfile=os.path.join("userdata", "vuzes.csv")
 	print("Saving vuzes...")
 	with open(vuzfile, "w", newline='', encoding="utf8") as f:
 		wr=csv.writer(f, delimiter=',', quotechar='"')
-		for row in vuzes:
+		for row in results:
 			#print(row)
 			print("*", end="")
 			wr.writerow(row)
 		print("")
 	print("Done")
-
-	vuzes_tr=[]
-	for i in vuzes:
-		vuzes_tr.append(i[1])
-	tree=wordgroup.gentree(vuzes_tr)
+	###
+	result_tr=[]
+	for i in results:
+		result_tr.append(i[1])
+	tree=wordgroup.gentree(result_tr)
 	wordgroup.showtree(tree)
+
